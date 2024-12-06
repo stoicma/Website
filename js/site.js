fa20228eb91e1b19c55486517babb382a4a4403e
MarkoStoic <marko.stokic@proton.me>
2024-12-06 17:51:14 +0100

    Relaunch Website

--- js/site.js
+++ js/site.js
@@ -66,7 +66,41 @@ $(window).load(function () { // makes sure the whole site is loaded
 
 });
 
+
+let bookReviews = {
+    "Atomic Habits": "It really helped me nail down my routines! Main takeaways: try to stack habits; they are always part of a system; identity > outcome; make it easy to follow good habits",
+    "She who became the sun": "Amazing fantasy book about a little girl that is forced to change identity.",
+    // Add more books and their reviews here
+};
+
 /* Showing and then removing hearts after a button is clicked */
+
+document.addEventListener('DOMContentLoaded', function() {
+		// Get the modal
+	var modal = document.getElementById("bookReviewModal");
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+	// When the user clicks on the book, open the modal
+	document.querySelectorAll('.book-images').forEach(book => {
+		book.onclick = function() {
+			const bookId = this.getAttribute("data-book-id");
+			const reviewText = bookReviews[bookId]; // Retrieve review using bookId
+			document.getElementById("bookReviewText").innerText = reviewText; // Set the review text
+			modal.style.display = "block";
+		}
+	})
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+	modal.style.display = "none";
+		}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+	}
+});
 
 document.getElementById("button-love").addEventListener("click", function() {
     const heart = document.createElement("div");
@@ -81,4 +115,24 @@ document.getElementById("button-love").addEventListener("click", function() {
 		heart.remove();
 	}, 2000); // Adjust time to match the longest possible animation duration
 });
+
+/* Showing and then removing books after a button is clicked */
+
+document.getElementById("button-books").addEventListener("click", function() {
+    const book = document.createElement("div");
+    book.classList.add("book");
+	console.log(book);
+    book.style.left = Math.random() * 100 + "vw";
+	book.style.top = Math.random()* 100 + "vh";
+    book.style.animationDuration = Math.random() * 2 + 1 + "s";
+    document.body.appendChild(book); 
+	// Remove book after animation completes
+	setTimeout(() => {
+		book.remove();
+	}, 2000); // Adjust time to match the longest possible animation duration
+});
+
+
+
+
 
